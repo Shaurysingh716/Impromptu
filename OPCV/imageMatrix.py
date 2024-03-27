f2ccfe78b6ce1eebd4ed45ec76f5f4bd07223c3d
Shaurysingh716 <shaurysingh2134@gmail.com>
2024-03-27 15:33:28 +0530

    Edge Detection

--- OPCV/imageMatrix.py
+++ OPCV/imageMatrix.py
@@ -1,21 +1,21 @@
 import cv2
 import numpy as np
+horizontal_kernel = np.array([[-1, -2, -1],[0, 0, 0],[1, 2, 1]])
+vertical_kernel = np.array([[-1, 0, 1],[-2, 0, 2],[-1, 0, 1]])
 
-# Load the image
 image = cv2.imread('C:\\Users\\ASUS\\Desktop\\Impromptu\\Impromptu\\OPCV\\Dataset\\Image1.jpg', cv2.IMREAD_GRAYSCALE)
 
-# Apply Sobel edge detection to find horizontal and vertical edges
-sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
-sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+horizontal_edges = cv2.filter2D(image, -1, horizontal_kernel)
+vertical_edges = cv2.filter2D(image, -1, vertical_kernel)
 
-# Threshold the gradient magnitude
-magnitude_threshold = 100
-sobelx = np.uint8(np.absolute(sobelx))
-sobely = np.uint8(np.absolute(sobely))
-combined_edges = cv2.bitwise_or(sobelx, sobely)
+combined_edges = cv2.addWeighted(horizontal_edges, 0.5, vertical_edges, 0.5, 0)
 
-# Display the result
-cv2.imshow('Original Image', image)
-cv2.imshow('Combined Edges', combined_edges)
+# cv2.imshow('Original Image', image)
+# cv2.imshow('Combined Edges', combined_edges)
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+faces = face_cascade.detectMultiScale(combined_edges, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+cv2.imshow('Face Detection', combined_edges)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
